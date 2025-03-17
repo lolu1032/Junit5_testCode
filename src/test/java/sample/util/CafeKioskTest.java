a6926b9cc4693057a0c30ecf24ebbb21b9ce6b43
lolu1032 <soowkw@naver.com>
2025-03-17 21:42:13 +0900

    BDD style

--- src/test/java/sample/util/CafeKioskTest.java
+++ src/test/java/sample/util/CafeKioskTest.java
@@ -1,9 +1,9 @@
 package sample.util;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.util.beverage.Americano;
 import sample.util.beverage.Latte;
-import sample.util.order.Order;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +21,8 @@ class CafeKioskTest {
         System.out.println(">>>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+    //    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -77,8 +79,11 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -86,8 +91,10 @@ class CafeKioskTest {
         cafeKiosk.add(americano, 1);
         cafeKiosk.add(latte, 1);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+        // then
         assertThat(totalPrice).isEqualTo(8500);
 
     }
@@ -115,4 +122,6 @@ class CafeKioskTest {
                 .hasMessage("주문 시간이 아닙니다. 관리자에게 문의 주세요");
 
     }
+
+
 }
